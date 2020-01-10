5caa4cb820dc7f68ba53fcad0dec9b8e332a40d4
Alex J. Best <alex.j.best@gmail.com>
2020-01-10 17:42:04 +0530

    added some benchmarks

---
+++ test/bench.jl
@@ -0,0 +1,28 @@
+using Coleman
+using Nemo
+
+a = 3
+
+R, x = PolynomialRing(Nemo.GF(257), "x")
+h = x^8 + x^7 + 1321*x^5 + 12321*x + 1765765
+@time ZetaFunction(a, h) # will include compile time
+@time ZetaFunction(a, h)
+@time ZetaFunction(a, h)
+
+R, x = PolynomialRing(Nemo.GF(521), "x")
+h = x^8 + x^7 + 1321*x^5 + 12321*x + 1765765
+@time ZetaFunction(a, h)
+@time ZetaFunction(a, h)
+@time ZetaFunction(a, h)
+
+R, x = PolynomialRing(Nemo.GF(1031), "x")
+h = x^8 + x^7 + 1321*x^5 + 12321*x + 1765765
+@time ZetaFunction(a, h)
+@time ZetaFunction(a, h)
+@time ZetaFunction(a, h)
+
+R, x = PolynomialRing(Nemo.FiniteField(137,2,"s")[1],"x")
+h = x^8 + x^7 + 1321*x^5 + 12321*x + 1765765
+@time ZetaFunction(a, h)
+@time ZetaFunction(a, h)
+@time ZetaFunction(a, h)
