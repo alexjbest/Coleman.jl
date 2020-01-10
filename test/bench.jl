a0b0e565abfa6f2a65484e7695aad25872947ab3
Alex J. Best <alex.j.best@gmail.com>
2020-01-10 17:45:20 +0530

    a degree 2 also

--- test/bench.jl
+++ test/bench.jl
@@ -3,9 +3,12 @@ using Nemo
 
 a = 3
 
+
 R, x = PolynomialRing(Nemo.GF(257), "x")
 h = x^8 + x^7 + 1321*x^5 + 12321*x + 1765765
 @time ZetaFunction(a, h) # will include compile time
+print("Compilation done")
+@time ZetaFunction(a, h)
 @time ZetaFunction(a, h)
 @time ZetaFunction(a, h)
 
@@ -21,8 +24,10 @@ h = x^8 + x^7 + 1321*x^5 + 12321*x + 1765765
 @time ZetaFunction(a, h)
 @time ZetaFunction(a, h)
 
-R, x = PolynomialRing(Nemo.FiniteField(137,2,"s")[1],"x")
+R, x = PolynomialRing(Nemo.FiniteField(211,2,"s")[1],"x")
 h = x^8 + x^7 + 1321*x^5 + 12321*x + 1765765
+@time ZetaFunction(a, h) # will include compile time
+print("Compilation done")
 @time ZetaFunction(a, h)
 @time ZetaFunction(a, h)
 @time ZetaFunction(a, h)
