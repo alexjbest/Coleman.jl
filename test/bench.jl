4a2e4bed57b384109fbcf8d211f895e773992868
Alex J. Best <alex.j.best@gmail.com>
2020-01-11 13:21:07 +0530

    update bench

--- test/bench.jl
+++ test/bench.jl
@@ -3,11 +3,18 @@ using Nemo
 
 a = 3
 
+R, x = PolynomialRing(Nemo.GF(2^32 + 15), "x")
+h = x^5 + x^4 + 12321*x + 1765765
+@time ZetaFunction(a, h) # will include compile time
+println("Compilation done")
+@time ZetaFunction(a, h)
+@time ZetaFunction(a, h)
+@time ZetaFunction(a, h)
 
 R, x = PolynomialRing(Nemo.GF(257), "x")
 h = x^8 + x^7 + 1321*x^5 + 12321*x + 1765765
 @time ZetaFunction(a, h) # will include compile time
-print("Compilation done")
+println("Compilation done")
 @time ZetaFunction(a, h)
 @time ZetaFunction(a, h)
 @time ZetaFunction(a, h)
@@ -24,10 +31,22 @@ h = x^8 + x^7 + 1321*x^5 + 12321*x + 1765765
 @time ZetaFunction(a, h)
 @time ZetaFunction(a, h)
 
-R, x = PolynomialRing(Nemo.FiniteField(211,2,"s")[1],"x")
-h = x^8 + x^7 + 1321*x^5 + 12321*x + 1765765
+#=
+F, b = Nemo.FiniteField(211,2,"s")
+R, x = PolynomialRing(F,"x")
+h = x^8 + x^7 - b*1321*x^5 + b*12321*x + 1765765
 @time ZetaFunction(a, h) # will include compile time
-print("Compilation done")
+println("Compilation done")
+@time ZetaFunction(a, h)
+@time ZetaFunction(a, h)
+@time ZetaFunction(a, h)
+=#
+
+F = Nemo.GF(ZZ(2)^64 + 13)
+R, x = PolynomialRing(F,"x")
+h = x^4 + x^3 + 12321*x - 1765765
+@time ZetaFunction(a, h) # will include compile time?
+println("Compilation done")
 @time ZetaFunction(a, h)
 @time ZetaFunction(a, h)
 @time ZetaFunction(a, h)
--- test/bench.m
+++ test/bench.m
@@ -4,30 +4,30 @@
 a:= 3;
 
 
-R<x>:= PolynomialRing(Nemo.GF(257), "x");
+R<x>:= PolynomialRing(GF(257));
 h:= x^8 + x^7 + 1321*x^5 + 12321*x + 1765765;
-@time ZetaFunction(a, h); // will include compile time
-print("Compilation done");
-@time ZetaFunction(a, h);
-@time ZetaFunction(a, h);
-@time ZetaFunction(a, h);
+time ZetaFunction(a, h);
+print("ok");
+time ZetaFunction(a, h);
+time ZetaFunction(a, h);
+time ZetaFunction(a, h);
 
-R, x:= PolynomialRing(Nemo.GF(521), "x");
+R<x>:= PolynomialRing(GF(521));
 h:= x^8 + x^7 + 1321*x^5 + 12321*x + 1765765;
-@time ZetaFunction(a, h);
-@time ZetaFunction(a, h);
-@time ZetaFunction(a, h);
+time ZetaFunction(a, h);
+time ZetaFunction(a, h);
+time ZetaFunction(a, h);
 
-R, x:= PolynomialRing(Nemo.GF(1031), "x")
-h:= x^8 + x^7 + 1321*x^5 + 12321*x + 1765765
-@time ZetaFunction(a, h)
-@time ZetaFunction(a, h)
-@time ZetaFunction(a, h)
+R<x>:= PolynomialRing(GF(1031));
+h:= x^8 + x^7 + 1321*x^5 + 12321*x + 1765765;
+time ZetaFunction(a, h);
+time ZetaFunction(a, h);
+time ZetaFunction(a, h);
 
-R, x:= PolynomialRing(Nemo.FiniteField(211,2,"s")[1],"x")
-h:= x^8 + x^7 + 1321*x^5 + 12321*x + 1765765
-@time ZetaFunction(a, h) # will include compile time
-print("Compilation done")
-@time ZetaFunction(a, h)
-@time ZetaFunction(a, h)
-@time ZetaFunction(a, h)
+R<x>:= PolynomialRing(GF(211^2));
+h:= x^8 + x^7 + 1321*x^5 + 12321*x + 1765765;
+time ZetaFunction(a, h);
+print("ok");
+time ZetaFunction(a, h);
+time ZetaFunction(a, h);
+time ZetaFunction(a, h);
